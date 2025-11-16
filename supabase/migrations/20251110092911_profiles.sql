b357a47b108503d0a03a939efc90648f2c72d9db
pariszhuang <pariszhuang@gmail.com>
2025-11-16 22:00:55 +1300

    Search Path not correct

--- supabase/migrations/20251110092911_profiles.sql
+++ supabase/migrations/20251110092911_profiles.sql
@@ -35,7 +35,7 @@ CREATE OR REPLACE FUNCTION public.handle_new_user()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 SECURITY DEFINER
-SET search_path = ""
+SET search_path = ''
 AS $$
 DECLARE
   default_avatar UUID;
