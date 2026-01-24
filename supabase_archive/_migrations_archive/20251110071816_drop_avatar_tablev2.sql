3739e116ff8817aa10a89bd7558798e95c9908c6
pariszhuang <pariszhuang@gmail.com>
2026-01-24 22:42:01 +1300

    Delete unnecessary supabase in Kinly App now migrate to kinly-backend

--- supabase_archive/_migrations_archive/20251110071816_drop_avatar_tablev2.sql
+++
@@ -1,2 +0,0 @@
--- If a table exists from earlier attempts, drop it so we rebuild cleanly
-drop table if exists public.avatars cascade;
--- supabase_archive/migrations/20251119103000_drop_stub_profiles_delete_account.sql
+++
@@ -1,2 +0,0 @@
--- Drop stub RPC that should not be part of the public API
-drop function if exists public.profiles_delete_account();
